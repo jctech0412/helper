054c082a5738de27dc2da28caa2706edb2fe7749
FlyDev515 <142128771+Anderson515@users.noreply.github.com>
2023-09-01 14:31:21 +0900

    first

---
+++ bid_text/wju/bid/vue.js
@@ -0,0 +1,10 @@
+https://www.entradasoft.com/
+https://www.jeprent.ee/
+https://globalprompt.com.mx/
+https://sedo.com/
+https://www.loetjezalen.nl/
+https://www.bkt-kabeltechnik.de/
+
+------------*------------*------------*------------*------------*------------*------------*------------*------------*------------*------------
+
+Vue.js, Node.js, PHP, HTML, CSS, SASS, JavaScript, TypeScript, MongoDB, SQL, MySQL, PostgreSQL 
