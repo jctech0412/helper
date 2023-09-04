c1468f5df39c5cb57818222c2d7fcad88eccceb6
jc <jctech0412@gmail.com>
2023-09-04 18:08:05 +0800

    update

--- bid_text/wju/bid/nuxt.js
+++
@@ -1,7 +0,0 @@
-https://www.zeelool.com/
-https://www.getshopwave.com/
-https://squarelovin.com/
-
-------------*------------*------------*------------*------------*------------*------------*------------*------------*------------*------------
-
-React.js, Nuxt.js, Node.js, PHP, HTML, CSS, SASS, JavaScript, TypeScript, MongoDB, SQL, MySQL, PostgreSQL 
